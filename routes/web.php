2512ac0926d0d113991a87bffb9f4f74305c7ff1
TrongTin <trantrongtin2005@gmail.com>
2026-04-15 05:07:44 +0700

    create file routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CrudUserController;
 
-Route::get('/', [CrudUserController::class, 'listUser']);
+Route::get('/', [CrudUserController::class, 'listUser'])->name('home');
 Route::get('/list', [CrudUserController::class, 'listUser'])->name('list');
-Route::get('/users', [CrudUserController::class, 'listUser'])->name('user.list');
+Route::get('/signout', function () {
+    return redirect()->route('user.list');
+})->name('signout');
+
+Route::prefix('users')->name('user.')->group(function () {
+    Route::get('/', [CrudUserController::class, 'listUser'])->name('list');
+    Route::get('/{user}', [CrudUserController::class, 'readUser'])->name('readUser');
+    Route::get('/{user}/edit', [CrudUserController::class, 'editUser'])->name('updateUser');
+    Route::put('/{user}', [CrudUserController::class, 'saveUser'])->name('saveUser');
+    Route::delete('/{user}', [CrudUserController::class, 'deleteUser'])->name('deleteUser');
+});
